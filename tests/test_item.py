398bd478ab3e8105c9dbabc735a85eef1c23c46f
Arina <arinafedor23@icloud.com>
2023-09-27 20:37:57 +0300

    add code in item and item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item('test1', 20.0, 5)
+    assert item1.calculate_total_price() == 100.0
+
+def test_apply_discount():
+    item1 = Item('test2', 20.0, 5)
+    item1.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 10
