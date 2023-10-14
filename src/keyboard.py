97c93ae0bdd45e3fa29103468f01615ddff3d86c
Arina <arinafedor23@icloud.com>
2023-10-14 11:14:04 +0300

    add keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,21 +4,26 @@ from src.item import Item
 class MixinLang:
     def __init__(self):
         language = "EN"
-        self.language = language
+        self.__language = language
+
+    @property
+    def language(self):
+        return self.__language
+
 
     def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
+        if self.__language == "EN":
+            self.__language = "RU"
         else:
-            self.language = "EN"
+            self.__language = "EN"
 
     def save_lang(self):
-        print(f'Язык клавиатуры - {self.language}')
+        print(f'Язык клавиатуры - {self.__language}')
 
 
 class Keyboard(Item, MixinLang):
     def __init__(self, name: str, price: float, quantity: int, languange="EN"):
         super().__init__(name, price, quantity)
-        self.language = languange
+        self.__language = languange
         if self.language.upper() != "EN" and self.language.upper() != "RU":
             raise ValueError('язык клавиатуры может быть только EN или RU')
